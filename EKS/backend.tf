1b932d7a4100119315aee57cbd13e2c36bc8f6ee
karthikkaveri93 <154875995+karthikkaveri93@users.noreply.github.com>
2024-03-28 13:21:12 +0530

    Update backend.tf
    adding us-south-1 for backend changes

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "cicd-terraform-eks"
+    bucket = "karthikbucket-new-123456789"   
     key    = "eks/terraform.tfstate"
-    region = "us-east-1"
+    region = "ap-south-1"
   }
 }
